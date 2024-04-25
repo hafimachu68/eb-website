a53ef511afe71a21557524b79f7890e7ac1772c5
hafe123 <hafeeza@expressbase.com>
2024-04-25 11:27:11 +0530

    latest changes

--- src/Components/common/Mcarousel.jsx
+++ src/Components/common/Mcarousel.jsx
@@ -7,7 +7,7 @@ import './css/mform.css';
 
 function Mcarousel() {
   return (
-    <div style={{ width: '100%', height: '48vh' }}>
+    <div style={{ width: '100%', height: '68vh' }}>
       <Carousel className="custom-carousel" style={{ width: '100%', height: '100%' }}>
         <Carousel.Item>
           <Card className="mbg text-light">
