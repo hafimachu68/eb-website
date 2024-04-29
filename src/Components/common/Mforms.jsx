51b4052c99c4fb9b7f605fe7e6a2e4c34ad3239a
hafe123 <hafeeza@expressbase.com>
2024-04-29 12:32:44 +0530

    web  updates

--- src/Components/common/Mforms.jsx
+++ src/Components/common/Mforms.jsx
@@ -27,44 +27,36 @@ function Mforms() {
                     </Col>
                   </Row>                  
                   <div className="carousel-content pt-5">
-                    <Row>
-                      <Col>
-                        <h5>Sub Heading</h5>
-                      </Col>
-                      <Col className="text-center align-self-center">
-                        <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
-                      </Col>
-                    </Row>
-                    <h1>Main Heading</h1>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    
+                        <h5 className='mmh'>Build complex enterprise class Web Forms 10x faster</h5>
+                         <p className='mmp'> Create powerful multi-lingual,multi-currency enterprise web forms with built-in audit trail, infinite validations and RBAC security.       
                         </p>
+                        <div className=" align-self-center mbd">
+                        <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
+                      </div>
                     <img className='mmg' src={mainimg} alt="Slide 1" />
                   </div>
                 </Card.Body>
               </Card>
             </Carousel.Item>
             <Carousel.Item>
-              <Card className="carousel-card pt-5">
+              <Card className="carousel-card pt-4">
               <Card.Body>
-              <Row>
+                <Row>
                     <Col>
-                      <img  src={report} alt="Web Forms Icon" className="icon" /> {/* Web Forms Icon */}
+                      <img  src={report} alt="report Forms Icon" className="icon" /> 
                     </Col>
                     <Col className="text-center align-self-center">
                     <h2 className='mch text-center'>Reports</h2>
                     </Col>
-                  </Row>                         
-                   <div className="carousel-content pt-5">
-                  <Row>
-                      <Col>
-                        <h5>Sub Heading</h5>
-                      </Col>
-                      <Col className="text-center align-self-center">
-                        <Button   className="mbtn" variant="light">Explore</Button>{/* Explore button */}
-                      </Col>
-                    </Row>                    <h1>Main Heading</h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    </p>
+                  </Row>                  
+                  <div className="carousel-content pt-5">
+                    
+                        <h5 className='mmh'>Low-Code Reporting tools for Modern Businesses to accelerate decision making</h5>
+                         <p className='mmp'> Gain instant access to actionable insights, putting data at your fingertips for informed decision-making.     </p>
+                        <div className=" align-self-center mbd">
+                        <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
+                      </div>
                     <img className='mmg' src={mainimg} alt="Slide 1" />
                   </div>
                 </Card.Body>
@@ -73,25 +65,22 @@ function Mforms() {
             <Carousel.Item>
               <Card className="carousel-card pt-5">
               <Card.Body>
-              <Row>
+                <Row>
                     <Col>
-                      <img  src={documents} alt="Web Forms Icon" className="icon" /> {/* Web Forms Icon */}
+                      <img  src={documents} alt="Documents Forms Icon" className="icon" /> {/* Web Forms Icon */}
                     </Col>
                     <Col className="text-center align-self-center">
                     <h2 className='mch text-center'>Documents</h2>
                     </Col>
-                  </Row>  
-                 <div className="carousel-content pt-5">
-                  <Row>
-                      <Col>
-                        <h5>Sub Heading</h5>
-                      </Col>
-                      <Col className="text-center align-self-center">
+                  </Row>                  
+                  <div className="carousel-content pt-5">
+                    
+                        <h5 className='mmh'>Document Management for business apps like never before</h5>
+                         <p className='mmp'>A document management system that seamlessly integrates into your business process. Say goodbye to toggling between platforms—manage documents in-context, ensuring seamless access to crucial information.
+                         </p>
+                        <div className=" align-self-center mbd">
                         <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
-                      </Col>
-                    </Row>                    <h1>Main Heading</h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    </p>
+                      </div>
                     <img className='mmg' src={mainimg} alt="Slide 1" />
                   </div>
                 </Card.Body>
@@ -100,25 +89,22 @@ function Mforms() {
             <Carousel.Item>
               <Card className="carousel-card pt-5">
               <Card.Body>
-              <Row>
+                <Row>
                     <Col>
-                      <img  src={api} alt="Web Forms Icon" className="icon" /> {/* Web Forms Icon */}
+                      <img  src={api} alt="APIs Forms Icon" className="icon" /> {/* Web Forms Icon */}
                     </Col>
                     <Col className="text-center align-self-center">
                     <h2 className='mch text-center'>APIs</h2>
                     </Col>
-                  </Row>        
-                <div className="carousel-content pt-5">
-                  <Row>
-                      <Col>
-                        <h5>Sub Heading</h5>
-                      </Col>
-                      <Col className="text-center align-self-center">
+                  </Row>                  
+                  <div className="carousel-content pt-5">
+                    
+                        <h5 className='mmh'>Seamlessly Integrate with 3rd Party software</h5>
+                         <p className='mmp'> Efficiently integrate third-party applications into your workflow using our futuristic API builder. Simplify cross-platform connectivity and unlock new possibilities for productivity and growth.
+                         </p>
+                        <div className=" align-self-center mbd">
                         <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
-                      </Col>
-                    </Row>                    <h1>Main Heading</h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    </p>
+                      </div>
                     <img className='mmg' src={mainimg} alt="Slide 1" />
                   </div>
                 </Card.Body>
@@ -127,25 +113,22 @@ function Mforms() {
             <Carousel.Item>
               <Card className="carousel-card pt-5">
               <Card.Body>
-              <Row>
+                <Row>
                     <Col>
-                      <img  src={mobapp} alt="Web Forms Icon" className="icon" /> {/* Web Forms Icon */}
+                      <img  src={mobapp} alt="Mobile Apps Forms Icon" className="icon" /> {/* Web Forms Icon */}
                     </Col>
                     <Col className="text-center align-self-center">
-                    <h2 className='mch text-center'>Mobile App</h2>
+                    <h2 className='mch text-center'>Mobile Apps</h2>
                     </Col>
-                  </Row>        
+                  </Row>                  
                   <div className="carousel-content pt-5">
-                  <Row>
-                      <Col>
-                        <h5>Sub Heading</h5>
-                      </Col>
-                      <Col className="text-center align-self-center">
+                    
+                        <h5 className='mmh'>Empower Your fast growing business with Internal Mobile Appsr</h5>
+                         <p className='mmp'> Transform your organization's internal processes with our low-code mobile app platform. From task management to internal communications, our apps empower your team to collaborate seamlessly and stay productive from anywhere.
+                         </p>
+                        <div className=" align-self-center mbd">
                         <Button className="mbtn"  variant="light">Explore</Button>{/* Explore button */}
-                      </Col>
-                    </Row>                    <h1>Main Heading</h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    </p>
+                      </div>
                     <img className='mmg' src={mainimg} alt="Slide 1" />
                   </div>
                 </Card.Body>
