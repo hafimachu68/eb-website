abc0486dab4dc1067c7fb645da91d80ff5351ad1
hafe123 <hafeeza@expressbase.com>
2024-11-01 13:05:38 +0400

    new update commit

--- src/Components/common/Contactus.jsx
+++ src/Components/common/Contactus.jsx
@@ -1,109 +1,104 @@
-import React from 'react'
+import React from 'react';
 import './css/contact.css';
-import cnt1  from '../images/cnt1.png'; // Import your logo file
-import cnt2  from '../images/cnt2.png'; // Import your logo file
-import cnt3  from '../images/cnt3.png'; // Import your logo file
-import cnt4  from '../images/cnt4.png'; // Import your logo file
-import cnt5  from '../images/cnt5.png'; // Import your logo file
-import cnt6  from '../images/cnt6.png'; // Import your logo file
-import cnt7  from '../images/cnt7.png'; // Import your logo file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faXTwitter, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+import cnt1 from '../images/c13.svg';
+import cnt3 from '../images/c12.svg';
+import cnt2 from '../images/c11.svg';
 import Cform from './Cform';
-import background from '../images/cntct.png'; // Import your image
-import bg from '../images/phone.png'; // Import your image
-
-
-
+import background from '../images/cntct.png';
 
 function Contactus() {
   return (
     <div>
-         <div className="contact-header"style={{backgroundImage: `url(${background})`}}>
-      <div className="contact-overlay">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 no-padd al_bottom">
-              <h1 className="ch fw-bold">Contact Us </h1>
-              <p className="  " >
-              Your path to streamlined efficiency starts here.           </p>
+      <div className="contact-header" style={{ backgroundImage: `url(${background})` }}>
+        <div className="contact-overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 no-padd al_bottom">
+                <h1 className="ch fw-bold">Contact Us</h1>
+                <p>Your path to streamlined efficiency starts here.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container cnlay">
+
+      <div className="container cnlay">
         <div className="row">
-            <div className="col-md-6">
-                <div className='container locate'>
-                <div className="row pt-3">
-                    <div className="col-md-2">
-                    <i> <img className='loc mx-5' src={cnt1} alt="" /> </i>
-                    </div>
-                    <div className="col-md-10">
-                        <p>
-                        Integrated Startup Complex, KINFRA Hi-Tech Park, Kalamassery, Kochi 683503, INDIA.                          </p>
-                    </div>
-                  </div>
-                  <div className="row pt-3">
-                    <div className="col-md-2">
-                    <i> <img className='pv mx-5' src={cnt2} alt="" /> </i>
-                    </div>
-                    <div className="col-md-10">
-                        <p>+91 85890 71379</p>
-                    </div>
-                  </div>
-                  <div className="row pt-3">
-                    <div className="col-md-2">
-                    <i> <img className='pv mx-5' src={cnt3} alt="" /> </i>
-                    </div>
-                    <div className="col-md-10">
-                        <p>www.expressbase.com</p>
-                    </div>
-                  </div>
-                  <div className="row pt-3">
-                    <div className="col-md-2">
-                    <i> <img className='pv mx-5' src={cnt4} alt="" /> </i>
-                    </div>
-                    <div className="col-md-2 ">
-                    <i> <img className='pv  ' src={cnt5} alt="" /> </i>
-                    </div>
-                    <div className="col-md-2">
-                    <i> <img className='pv tw' src={cnt6} alt="" /> </i>
-                    </div>
-                    <div className="col-md-2">
-                    <i> <img className='pv ytb' src={cnt7} alt="" /> </i>
-                    </div>
-                    
-                  </div>
+          <div className="col-md-6">
+            <div className="container locate">
+              <div className="row pt-3">
+                <div className="col-md-2">
+                  <i><img className='loc mx-5' src={cnt1} alt="" /></i>
                 </div>
-                <a href="https://maps.app.goo.gl/M5pGVp96fud5Sx2RA" target="_blank" rel="noopener noreferrer">
+                <div className="col-md-10">
+                  <p>Integrated Startup Complex, KINFRA Hi-Tech Park, Kalamassery, Kochi 683503, INDIA.</p>
+                </div>
+              </div>
+              <div className="row pt-3">
+                <div className="col-md-2">
+                  <i><img className='pv mx-5' src={cnt2} alt="" /></i>
+                </div>
+                <div className="col-md-10">
+                  <p>+91 85890 71379</p>
+                </div>
+              </div>
+              <div className="row pt-3">
+                <div className="col-md-2">
+                  <i><img className='pv mx-5' src={cnt3} alt="" /></i>
+                </div>
+                <div className="col-md-10">
+                  <p>www.expressbase.com</p>
+                </div>
+              </div>
+              <div className="d-flex justify-content-normal align-items-center icon-row mt-5" style={{ marginLeft: '3rem' }}>
+                <a href="https://www.facebook.com/expressbase" className="text-reset ccircle-icon me-3">
+                  <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+                <a href="https://x.com/expressbasesys" className="text-reset ccircle-icon me-3">
+                  <FontAwesomeIcon icon={faXTwitter} />
+                </a>
+                <a href="https://www.linkedin.com/company/expressbase-systems-private-limited/" className="text-reset ccircle-icon me-3">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="https://www.youtube.com/@expressbase2443" className="text-reset ccircle-icon me-3">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href="https://www.instagram.com/expressbasesys?igsh=MWRyYWRlN3l4NDRldg==" className="text-reset ccircle-icon me-3">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </div>
+            </div>
 
-                <div className='container phone'style={{backgroundImage: `url(${bg})`}}>
-                <div className="contact-overlay">
-        <div className="container lc">
-          <div className="row">
-            <div className="col-md-12 no-padd ">
-              <h2 className=" fw-bold">Locate Us </h2>
+            {/* Embedded Google Map */}
+            <div className='container map-container'>
+              <h2 className="fw-bold">Locate Us</h2>
+              <iframe
+                title="Google Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5330414481523!2d76.35196277407839!3d10.055337172082005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c9d1555556b%3A0x29f4bf576cb3e547!2sEXPRESSbase%20Systems%20Private%20Limited!5e0!3m2!1sen!2sin!4v1730451156304!5m2!1sen!2sin" 
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+              
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <div className='container cform '>
+              <h4>Drop in a text</h4>
+              <Cform />
             </div>
           </div>
         </div>
       </div>
-     </div>
-     </a>
-            </div>
-            <div className="col-md-6">
-            <div className='container cform '>
-                <h4>Drop in a text</h4>
-                <Cform/>
-            </div>
-
-            </div>
-
-        </div>
-
-</div>
-
     </div>
-  )
+  );
 }
 
-export default Contactus
+export default Contactus;
--- src/Components/common/MContactus.jsx
+++ src/Components/common/MContactus.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './css/contact.css';
-import cnt1  from '../images/cnt1.png'; // Import your logo file
-import cnt2  from '../images/cnt2.png'; // Import your logo file
-import cnt3  from '../images/cnt3.png'; // Import your logo file
+import cnt1  from '../images/c13.svg'; // Import your logo file
+import cnt3  from '../images/c12.svg'; // Import your logo file
+import cnt2  from '../images/c11.svg'; // Import your logo file
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram,faXTwitter, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import insta  from '../images/insta.svg'; // Import your logo file
@@ -60,20 +60,20 @@ function MContactus() {
         <p>www.expressbase.com</p>
     </div>
 </div>
-<div className="d-flex justify-content-between align-items-center icon-row">
-    <a href="https://www.facebook.com/expressbase" className="text-reset circle-icon me-3">
+<div className="d-flex justify-content-between align-items-center icon-row mt-4">
+    <a href="https://www.facebook.com/expressbase" className="text-reset ccircle-icon me-3">
         <FontAwesomeIcon icon={faFacebookF} />
     </a>
-    <a href="https://x.com/expressbasesys" className="text-reset circle-icon me-3">
+    <a href="https://x.com/expressbasesys" className="text-reset ccircle-icon me-3">
         <FontAwesomeIcon icon={faXTwitter} />
     </a>
-    <a href="https://www.linkedin.com/company/expressbase-systems-private-limited/" className="text-reset circle-icon me-3">
+    <a href="https://www.linkedin.com/company/expressbase-systems-private-limited/" className="text-reset ccircle-icon me-3">
         <FontAwesomeIcon icon={faLinkedin} />
     </a>
-    <a href="https://www.youtube.com/@expressbase2443" className="text-reset circle-icon me-3">
+    <a href="https://www.youtube.com/@expressbase2443" className="text-reset ccircle-icon me-3">
         <FontAwesomeIcon icon={faYoutube} />
     </a>
-    <a href="https://www.instagram.com/expressbasesys?igsh=MWRyYWRlN3l4NDRldg==" className="text-reset circle-icon me-3">
+    <a href="https://www.instagram.com/expressbasesys?igsh=MWRyYWRlN3l4NDRldg==" className="text-reset ccircle-icon me-3">
         <FontAwesomeIcon icon={faInstagram} />
     </a>
 </div>
@@ -82,20 +82,20 @@ function MContactus() {
 
 
                 </div>
-                <a href="https://maps.app.goo.gl/M5pGVp96fud5Sx2RA" target="_blank" rel="noopener noreferrer">
-
-                <div className='container mphone'style={{backgroundImage: `url(${bg})`}}>
-                <div className="contact-overlay">
-        <div className="container lc">
-          <div className="row">
-            <div className="col-md-12 no-padd ">
-              <h2 className=" fw-bold">Locate Us </h2>
+                <div className='container map-container'>
+              <h2 className="fw-bold">Locate Us</h2>
+              <iframe
+                title="Google Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5330414481523!2d76.35196277407839!3d10.055337172082005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c9d1555556b%3A0x29f4bf576cb3e547!2sEXPRESSbase%20Systems%20Private%20Limited!5e0!3m2!1sen!2sin!4v1730451156304!5m2!1sen!2sin" 
+                width="100%"
+                height="250"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+              
             </div>
-          </div>
-        </div>
-      </div>
-     </div>
-     </a>
             </div>
            
 
